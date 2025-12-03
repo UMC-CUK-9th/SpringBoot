0f083b21aa723e73e52d7b33e252f2d7d34f94a5
jsh424 <jsh911424@catholic.ac.kr>
2025-12-03 12:00:21 +0900

    Refactor/Chapter10
    authorization 추가

--- umc9th/src/main/java/com/example/umc9th/global/config/SwaggerConfig.java
+++ umc9th/src/main/java/com/example/umc9th/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.example.umc9th.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +15,13 @@ public class SwaggerConfig {
     public OpenAPI swaggerOpenAPI() {
 
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Auth"))
+                .components(new Components().addSecuritySchemes("Bearer Auth",
+                        new SecurityScheme()
+                                .name("Bearer Auth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
                 .info(new Info()
                         .title("Project API")
                         .description("Project Swagger API Docs")
